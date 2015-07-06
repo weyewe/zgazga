29e736cfab0d4daf62bf3f0e8835b64c06df1a52
weyewe1 <rajakuraemas@gmail.com>
2015-07-06 05:51:20 +0000

    added model for batch  source, batch source allocation, batch instance, and batch stock mutation

--- app/models/batch_stock_mutation.rb
+++ app/models/batch_stock_mutation.rb
@@ -1,2 +1,21 @@
 class BatchStockMutation < ActiveRecord::Base
+    
+    def self.create_object( params ) 
+        new_object = self.new
+        
+        new_object.source_class     =   params[:source_class] 
+        new_object.source_id        =   params[:source_id]
+        new_object.amount           =   params[:amount]
+        new_object.status           =   params[:status]  
+        new_object.mutation_date    =   params[:mutation_date]      
+        new_object.item_id          =   params[:item_id]    
+        new_object.description      =   params[:description]     
+        new_object.save
+        
+        return new_object 
+    end
+    
+    def delete_object
+      self.destroy 
+    end
 end
